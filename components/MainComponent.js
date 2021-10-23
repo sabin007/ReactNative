027cb89a5d718b989c3120bea156a4dbf6bacd63
Sabin <sabinbhandari002@gmail.com>
2021-10-23 07:38:21 -0700

    Network Info

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -9,7 +9,9 @@ import {
   StyleSheet,
   Text,
   ScrollView,
-  Image
+  Image,
+  Alert,
+  ToastAndroid
 } from 'react-native';
 import Login from './LoginComponent';
 import { createStackNavigator } from 'react-navigation-stack';
@@ -28,6 +30,7 @@ import {
   fetchPromotions,
   fetchPartners
 } from '../redux/ActionCreators';
+import NetInfo from '@react-native-community/netinfo';
 
 const mapDispatchToProps = {
   fetchCampsites,
@@ -338,7 +341,42 @@ class Main extends Component {
     this.props.fetchComments();
     this.props.fetchPromotions();
     this.props.fetchPartners();
+    NetInfo.fetch().then((connectionInfo) => {
+      Platform.OS === 'ios'
+        ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+        : ToastAndroid.show(
+            'Initial Network Connectvity Type: ' + connectionInfo.type,
+            ToastAndroid.LONG
+          );
+    });
+    this.unsubscribeNetInfo = NetInfo.addEventListener((connectionInfo) => {
+      this.handleConnectivityChange(connectionInfo);
+    });
   }
+
+  componentWillUnmount() {
+    this.unsubscribeNetInfo();
+  }
+  handleConnectivityChange = (connectionInfo) => {
+    let connectionMsg = 'You are now connected to an active newtwork';
+    switch (connectionInfo.type) {
+      case ' none':
+        connectionMsg = 'No network is active';
+        break;
+      case 'unknown':
+        connectionMsg = 'the newtwork connection state is now unknown';
+        break;
+      case 'cellular':
+        connectionMsg = 'You are now connected to a cellular network';
+        break;
+      case 'wifi':
+        connectionMsg = 'You are now connected to the wifi network';
+        break;
+    }
+    Platform.OS === 'ios'
+      ? Alert.alert('Connection.change', connectionMsg)
+      : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
+  };
 
   render() {
     return (
